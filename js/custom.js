167525f47f2cd5a6acdcedcbbfa64c5b413ec3a5
Arpan Maheshwari <maheshwariarpan828@gmail.com>
2022-12-31 22:03:25 +0530

    First Commit

--- js/custom.js
+++ js/custom.js
@@ -161,25 +161,25 @@
   /* ----------------------------------------------------------- */
   /*  HEADING ANIMATION
       /* ----------------------------------------------------------- */
-  const target = window.document.querySelectorAll('h6')[0]
-  const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
-  const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
-  const flickerAndColorText = text =>
-    text
-    .split('')
-    .map(flickerLetter)
-    .map(colorLetter)
-    .join('');
-  const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
-  neonGlory(target);
-  target.onclick = ({
-    target
-  }) => neonGlory(target);
+  // const target = window.document.querySelectorAll('h6')[0]
+  // const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+  // const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+  // const flickerAndColorText = text =>
+  //   text
+  //   .split('')
+  //   .map(flickerLetter)
+  //   .map(colorLetter)
+  //   .join('');
+  // const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+  // neonGlory(target);
+  // target.onclick = ({
+  //   target
+  // }) => neonGlory(target);
 })(jQuery);
 
 /* ----------------------------------------------------------- */
 /*  HEADING ANIMATION
-    /* ----------------------------------------------------------- */
+//     /* ----------------------------------------------------------- */
 async function init() {
   const node = document.querySelector("#type-text")
   await sleep(2000)
@@ -218,5 +218,43 @@ class TypeAsync extends HTMLSpanElement {
 customElements.define('type-async', TypeAsync, {
   extends: 'span'
 })
+
+$(document).ready(function(){
+  
+  var mousePos = {};
+
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $(window).mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+  });
+  
+  $(window).mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
+  });
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 15;
+      
+      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
+      
+      var sizeInt = getRandomInt(10, 30);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
+
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+    }
+  }, 1);
+});
 
 init()
